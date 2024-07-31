2c7b9bd98262f3d9a124738ffff6fe161397dd42
ferdy <septioajivicky@gmail.com>
2024-07-31 07:40:55 +0700

    first commit

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+// navbar
+document.getElementById('menu-button').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('hidden');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto Condensed', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
